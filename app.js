ac0758550a2084347c91457705ee4ba670314c4b
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-01-29 08:32:56 -0600

    Added cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const {connectToDatabase} = require("./src/db/connection");
 const express = require('express');
-
+const cors = require('cors');
+const routes = require("./src/routes/index");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(routes);
+app.use(cors());
 
 
 // * db connection
